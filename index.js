161106d12bc1c1b441652d7568be8f521b80cfc4
thaixls <thaisprandina@gmail.com>
2025-08-14 20:49:24 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,41 @@
+
+let heroi = "Florzinha Meninas Super Poderosas"
+let xp = 10500
+let nivel = ""
+
+switch (true) {
+    case (xp < 1000):
+        nivel = "Ferro"
+        break
+
+    case (xp >= 1001 && xp <= 2000):
+        nivel = "Bronze"
+        break
+
+    case (xp >= 2001 && xp <= 5000):
+        nivel = "Prata"
+        break    
+
+    case (xp >= 5001 && xp <= 7000):
+        nivel = "Ouro"
+        break
+
+    case (xp >= 7001 && xp <= 8000):
+        nivel = "Platina"
+        break
+
+    case (xp >= 8001 && xp <= 9000):
+        nivel = "Ascendente"
+        break
+
+    case (xp >= 9001 && xp <= 10000):
+        nivel = "Imortal"
+        break
+        
+    case (xp >= 10001):
+        nivel = "Radiante"
+        break
+
+}
+
+console.log("O herói " + heroi + ", está no nível: " + nivel)
